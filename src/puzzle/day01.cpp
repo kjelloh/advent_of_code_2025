04b65df52487211e43bb44857edba62148b94092
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-12-01 08:29:00 +0100

    day01 part 2 example ok

--- src/puzzle/day01.cpp
+++ src/puzzle/day01.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <print>
 
-int solve(std::string file) {
+int solve_part_1(std::string file) {
   int result{};
 
   std::ifstream in{file};
@@ -34,7 +34,65 @@ int solve(std::string file) {
 
 }
 
+int modulo_100(int x) {
+  return ((x % 100) + 100) % 100; // keep C++ mod in 0..100 (wrap from negative)
+}
+
+int solve_part_2(std::string file) {
+  int result{};
+
+  std::ifstream in{file};
+
+  int current{50};
+  std::print(" Points at: {}",current);
+
+  std::string line{};
+  while (std::getline(in,line)) {
+    std::print("\nin:{}",line);
+
+    int dir = (line[0]=='R')?1:-1;
+    int val = std::stoi(line.substr(1));
+    std::print(" -> dir:{} val:{}",dir,val);
+
+    int delta = dir*val;
+    int next = current + delta;
+
+    int clicks{0};
+    if (next > 0) {
+      // We eneded up above zero. One click for each 100 interval
+      clicks = next / 100;
+      std::print("  >0 clicks: {}",clicks);
+    }
+    else if (next == 0) {
+      // We ended up at zero
+      result += 1;
+      std::print("  ==0 clicks: {}",clicks);
+    }
+    else if (next < 0) {
+      // We ended up below zero.
+      clicks = ((-next + 100) / 100);
+      if (current == 0) clicks += -1; // We already counted the zero 'crossing'
+      std::print(" <0 clicks: {}",clicks);
+    }
+
+    result += clicks;
+
+    current = modulo_100(next);
+
+    std::print(" Points at: {} total clicks: {}",current,result);
+
+  }
+
+  std::print("\npassword: {}",result);
+
+  return 0;
+
+}
+
+
 int day01() {
-  // return solve("example01.txt");
-  return solve("day01.txt");
+  // return solve_part_1("example01.txt");
+  // return solve_part_1("day01.txt");
+  return solve_part_2("example01.txt");
+  // return solve_part_1("day01.txt");
 }
