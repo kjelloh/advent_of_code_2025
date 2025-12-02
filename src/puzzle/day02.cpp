71d357355c6cfaa939898e4b935a64c1f311a129
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-12-02 06:23:32 +0100

    day02 parsing ok

--- src/puzzle/day02.cpp
+++ src/puzzle/day02.cpp
@@ -1,6 +1,31 @@
 #include <print>
+#include <iostream>
+#include <ranges>
+#include <fstream>
+#include <utility> // std::make_pair
+
+struct Entry
+{
+    std::string s;
+ 
+    friend std::istream &operator>>(std::istream &s, Entry &entry) {
+        std::getline(s, entry.s,',');
+        return s;
+    }
+}; // FileLine
 
 int day02() {
-  std::print("\nday02");  
+  std::print("\nday02");
+  std::ifstream in{"example02.txt"};
+  auto parsed = 
+      std::views::istream<Entry>(in)
+    | std::views::transform([](auto const& entry){
+        auto pos = entry.s.find('-');
+        return std::make_pair(entry.s.substr(0,pos),std::stoi(entry.s.substr(pos+1)));
+      });
+  for (auto const& entry : parsed) {
+    std::print("\n{} {} ",entry.first,entry.second);
+    
+  }
   return 0;
 }
