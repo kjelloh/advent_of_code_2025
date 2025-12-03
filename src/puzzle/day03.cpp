fcb4067f561d8dee9b2c2e2f99af653a04a969de
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-12-03 09:35:02 +0100

    day3 part 2 example ok

--- src/puzzle/day03.cpp
+++ src/puzzle/day03.cpp
@@ -7,11 +7,11 @@
 #include <numeric> // std::accumulate,...
 #include <set>
 
-int day03() {
+int p1() {
   size_t answer{};
   std::print("\nday03");
-  // std::ifstream in{"in.txt"};
-  std::ifstream in{"ex0.txt"};
+  std::ifstream in{"in.txt"};
+  // std::ifstream in{"ex0.txt"};
   std::string entry;
   while (std::getline(in,entry)) {
     std::print("\n{} ",entry);
@@ -30,6 +30,52 @@ int day03() {
   }
   std::print("\nANSWER: {}",answer);
 
-
   return 0;
+}
+
+int p2() {
+  size_t answer{};
+  std::print("\nday03");
+  std::ifstream in{"ex0.txt"};
+  // std::ifstream in{"in.txt"};
+  std::string entry;
+  while (std::getline(in,entry)) {
+    std::print("\n{} ",entry);
+
+
+    // We can solve this by finding the largest monotonic decerasing stack of digits from the input
+    auto to_remove = entry.size() - 12;
+    std::stack<char> stack{};
+
+    for (size_t i=0;i<entry.size();++i) {
+      while (not stack.empty() and to_remove > 0 and entry[i] > stack.top()) {
+        stack.pop();
+        --to_remove;
+      }
+      stack.push(entry[i]);
+    }
+
+    while (stack.size() > 12) stack.pop();
+
+    std::string picked{};
+    while (not stack.empty()) {
+      picked += stack.top();
+      stack.pop();
+    }
+    std::reverse(picked.begin(),picked.end());
+    auto optimized = std::stoll(picked);
+
+    std::print(" optimized:{}",optimized);
+
+    answer += optimized;
+
+  }
+  std::print("\nANSWER: {}",answer); // 3121910778619
+                                     // 3121910778619
+  return 0;
+}
+
+
+int day03() {
+  return p2();
 }
