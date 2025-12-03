2a54367b569b6e2222bde3c6b547106cf70ab338
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-12-03 09:38:53 +0100

    day3 part 1 and 2 ok

--- src/puzzle/day03.cpp
+++ src/puzzle/day03.cpp
@@ -7,7 +7,7 @@
 #include <numeric> // std::accumulate,...
 #include <set>
 
-int p1() {
+size_t p1() {
   size_t answer{};
   std::print("\nday03");
   std::ifstream in{"in.txt"};
@@ -28,16 +28,16 @@ int p1() {
     std::print(" picked {}{}",picked.first,picked.second);
     answer += (picked.first - '0')*10 + picked.second - '0';
   }
-  std::print("\nANSWER: {}",answer);
+  // std::print("\nANSWER: {}",answer);
 
-  return 0;
+  return answer;
 }
 
-int p2() {
+size_t p2() {
   size_t answer{};
   std::print("\nday03");
-  std::ifstream in{"ex0.txt"};
-  // std::ifstream in{"in.txt"};
+  // std::ifstream in{"ex0.txt"};
+  std::ifstream in{"in.txt"};
   std::string entry;
   while (std::getline(in,entry)) {
     std::print("\n{} ",entry);
@@ -70,12 +70,15 @@ int p2() {
     answer += optimized;
 
   }
-  std::print("\nANSWER: {}",answer); // 3121910778619
-                                     // 3121910778619
-  return 0;
+  // std::print("\nANSWER: {}",answer); // 3121910778619
+  //                                    // 3121910778619
+  return answer;
 }
 
 
 int day03() {
-  return p2();
+  auto answer1 = p1();
+  auto answer2 = p2();
+  std::print("\nANSWER p1:{}",answer1);
+  std::print("\nANSWER p2:{}",answer2);
 }
