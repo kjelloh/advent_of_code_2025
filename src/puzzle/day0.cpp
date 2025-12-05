01a37492633c03388fee1d3d3a46b5a6aa25b7d6
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-12-05 18:27:06 +0100

    Updated day0.cpp (template) from day6

--- src/puzzle/day0.cpp
+++ src/puzzle/day0.cpp
@@ -4,37 +4,48 @@
 #include <iostream>
 #include <fstream>
 #include <optional>
+#include <algorithm>
+
+using Model = std::vector<std::string>;
+Model parse(std::istream& in) {
+  Model model{};
+  std::string entry;
+  bool is_ranges{true};
+  while (std::getline(in,entry)) {
+    std::print("\n{} ",entry);
+    model.push_back(entry);
+  }
+  return model;
+}
 
 std::optional<size_t> p1(PuzzleArgs puzzle_args) {
   std::optional<size_t> answer{};
   std::ifstream in{puzzle_args.in_file_path()};
-  std::string entry;
-  while (std::getline(in,entry)) {
-    std::print("\n{} ",entry);
-  }
-
+  auto model = parse(in);
+  // Solve here
   return answer;
 }
 
 std::optional<size_t> p2(PuzzleArgs puzzle_args) {
   std::optional<size_t> answer{};
   std::ifstream in{puzzle_args.in_file_path()};
-  std::string entry;
-  while (std::getline(in,entry)) {
-    std::print("\n{} ",entry);
-  }
-
+  auto model = parse(in);
+  // Solve here
   return answer;
 }
 
-std::string day(PuzzleArgs puzzle_args) {
-  std::print("\nday04");
+std::optional<std::string> day(PuzzleArgs puzzle_args) {
+  std::print("\ndayxx");
   std::string result{};
-  if (auto answer = p1(puzzle_args)) {
-    result += std::format("p1:{}",*answer);
+  auto answer1 = p1(puzzle_args);
+  if (answer1) {
+    result += std::format("p1:{}",*answer1);
+    auto answer2 = p2(puzzle_args);
+    if (answer2) {
+      result += std::format("p1:{}",*answer2);      
+    }
   }
-  if (auto answer = p2(puzzle_args)) {
-    result += std::format(" p2:{}",*answer);
-  }
-  return result;
+
+  if (result.size() > 0) return result;
+  return std::nullopt;
 }
