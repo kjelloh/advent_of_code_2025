256139f031fcd0c44e50261ccf3a96f92d85eb68
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-12-06 22:13:58 +0100

    Updated day0 template from day6

--- src/puzzle/day0.cpp
+++ src/puzzle/day0.cpp
@@ -5,14 +5,16 @@
 #include <fstream>
 #include <optional>
 #include <algorithm>
+#include <sstream>
 
 using Model = std::vector<std::string>;
 Model parse(std::istream& in) {
   Model model{};
+
   std::string entry;
-  bool is_ranges{true};
-  while (std::getline(in,entry)) {
-    std::print("\n{} ",entry);
+  int ix{0};
+  while (std::getline(in, entry)) {
+    std::print("\n{}[0..{}]:'{}' ", ix++,entry.size()-1,entry);
     model.push_back(entry);
   }
   return model;
@@ -23,6 +25,8 @@ std::optional<size_t> p1(PuzzleArgs puzzle_args) {
   std::ifstream in{puzzle_args.in_file_path()};
   auto model = parse(in);
   // Solve here
+  size_t acc{};
+  // answer = acc;
   return answer;
 }
 
@@ -31,21 +35,30 @@ std::optional<size_t> p2(PuzzleArgs puzzle_args) {
   std::ifstream in{puzzle_args.in_file_path()};
   auto model = parse(in);
   // Solve here
+  size_t acc{};
+  // answer = acc;
   return answer;
 }
 
 std::optional<std::string> day(PuzzleArgs puzzle_args) {
-  std::print("\ndayxx debug:{}",puzzle_args.meta().m_debug);
-  std::string result{};
-  auto answer1 = p1(puzzle_args);
-  if (answer1) {
-    result += std::format("p1:{}",*answer1);
-    auto answer2 = p2(puzzle_args);
-    if (answer2) {
-      result += std::format("p1:{}",*answer2);      
-    }
+  std::print(
+    "\nday part:{} debug:{}"
+    ,puzzle_args.meta().m_part
+    ,puzzle_args.meta().m_debug);
+  
+  switch (puzzle_args.meta().m_part) {
+    case 1: {
+      return p1(puzzle_args)
+        .transform([](auto answer){
+          return std::format("{}",answer);
+        });
+    } break;
+    case 2:
+      return p2(puzzle_args)
+        .transform([](auto answer){
+          return std::format("{}",answer);
+        });
+      break;
   }
-
-  if (result.size() > 0) return result;
   return std::nullopt;
 }
