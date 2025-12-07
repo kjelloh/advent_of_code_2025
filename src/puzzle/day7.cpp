f92c777112566fced21f8bcb14e56836eb563851
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-12-07 06:59:24 +0100

    day7 part 1 example ok

--- src/puzzle/day7.cpp
+++ src/puzzle/day7.cpp
@@ -5,16 +5,25 @@
 #include <fstream>
 #include <optional>
 #include <algorithm>
-#include <sstream>
+#include <set>
 
-using Model = std::vector<std::string>;
+using Grid = std::vector<std::string>;
+
+void print(Grid const& grid) {
+  int ix{0};
+  for (auto const& row : grid) {
+    std::print("\n{:4}[0..{:3}]: '{}' ", ix++,row.size()-1,row);
+  }
+}
+
+using Model = Grid;
 Model parse(std::istream& in) {
   Model model{};
 
   std::string entry;
   int ix{0};
   while (std::getline(in, entry)) {
-    std::print("\n{}[0..{}]:'{}' ", ix++,entry.size()-1,entry);
+    std::print("\n{:4}[0..{:3}]: '{}' ", ix++,entry.size()-1,entry);
     model.push_back(entry);
   }
   return model;
@@ -24,9 +33,44 @@ std::optional<size_t> p1(PuzzleArgs puzzle_args) {
   std::optional<size_t> answer{};
   std::ifstream in{puzzle_args.in_file_path()};
   auto model = parse(in);
-  // Solve here
   size_t acc{};
-  // answer = acc;
+  // Solve here
+  int c = model[0].find('S');
+  int dr{1};
+  size_t num_rows = model.size();
+  size_t num_cols = model[0].size();
+
+  std::vector<std::set<int>> beams{
+     num_rows
+  };
+  beams[0].insert(c);
+
+  for (int r=1;r<model.size();++r) {
+    std::vector<int> hit_splitters{};
+    for (int c=0;c<num_cols;++c) {
+      if (beams[r-1].contains(c)) {
+        if (model[r][c] == '^') {
+          hit_splitters.push_back(c);
+          ++acc;
+        }
+        else beams[r].insert(c);
+      }
+    }
+    for (auto splitter : hit_splitters) {
+      if (splitter == 0) beams[r].insert(splitter+1);
+      else if (0 < splitter and splitter < num_cols-1) {
+        beams[r].insert(splitter-1);
+        beams[r].insert(splitter+1);
+      }
+      else beams[r].insert(splitter-1);
+    }
+    std::print(
+       "\n{} -> hit_splitters:{} beams:{}"
+      ,model[r],hit_splitters.size()
+      ,beams[r].size());
+  }
+
+  answer = acc;
   return answer;
 }
 
@@ -42,7 +86,7 @@ std::optional<size_t> p2(PuzzleArgs puzzle_args) {
 
 std::optional<std::string> day(PuzzleArgs puzzle_args) {
   std::print(
-    "\nday part:{} debug:{}"
+    "\nday7 part:{} debug:{}"
     ,puzzle_args.meta().m_part
     ,puzzle_args.meta().m_debug);
   
