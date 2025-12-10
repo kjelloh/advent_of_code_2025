60cd58e1bb05dac7407110df4b062333482e61ad
kjelloh <kjell-olov.hogdahl@itfied.se>
2025-12-10 21:48:04 +0100

    Updated day0 from day10

--- src/puzzle/day0.cpp
+++ src/puzzle/day0.cpp
@@ -8,6 +8,7 @@
 #include <set>
 #include <map>
 #include <sstream>
+#include <unordered_set>
 
 using INT = int64_t;
 using UINT = uint64_t;
@@ -26,7 +27,47 @@ Model parse(std::istream& in) {
   return model;
 }
 
-std::optional<std::string> p1(PuzzleArgs puzzle_args) {
+std::optional<std::string> test_p1(int i,int test_ix) {
+    if (test_ix > 0 and i==0) switch (test_ix) {
+      case 1: {
+      } break;
+    }
+
+    if (test_ix > 0 and i==1) switch (test_ix) {
+      case 2: {
+      } break;
+    };
+
+    if (test_ix > 0 and i==2) switch (test_ix) {
+      case 3: {
+      } break;
+    };
+
+  return {};    
+}
+
+std::optional<std::string> test_p2(int i,int test_ix) {
+    if (test_ix > 0 and i==0) switch (test_ix) {
+      case 1: {
+      } break;
+    }
+
+    if (test_ix > 0 and i==1) switch (test_ix) {
+      case 2: {
+      } break;
+    };
+
+    if (test_ix > 0 and i==2) switch (test_ix) {
+      case 3: {
+      } break;
+    };
+
+  return {};    
+}
+
+std::optional<std::string> solve(PuzzleArgs puzzle_args,bool for_part2 = false) {
+
+  auto test_ix = puzzle_args.meta().m_maybe_test.value_or(0);
 
   std::ifstream in{puzzle_args.in_file_path()};
   auto model = parse(in);
@@ -38,17 +79,20 @@ std::optional<std::string> p1(PuzzleArgs puzzle_args) {
   // return std::format("Not yet fully implemented");
   // return std::format("{}",candidate);
 
+} // solve
+
+
+std::optional<std::string> p1(PuzzleArgs puzzle_args) {
+  return solve(puzzle_args,false); 
 } // p1
 
 std::optional<std::string> p2(PuzzleArgs puzzle_args) {
-  return {};
-  // return std::format("Not yet fully implemented");
-  // return std::format("{}",candidate);
+  return solve(puzzle_args,true);
 } // p2
 
 std::optional<std::string> day(PuzzleArgs puzzle_args) {
   aoc::print(
-    "\nday part:{} debug:{}"
+    "\nday10 part:{} debug:{}"
     ,puzzle_args.meta().m_part
     ,puzzle_args.meta().m_debug);
   
